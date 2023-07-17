f9c18214950a0959e4908bb8a6b50c79402b2c81
Nermitor <dmatyukhin2006@mail.ru>
2023-07-17 12:12:55 +0700

    Хендлеры для вайфу

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -4,20 +4,33 @@ from waifu import ImageCategories
 
 from callbacks.waifu import WaifuCategoryCallbackData, WaifuTypeCallbackData
 
+
+# Клавиатура для sfw
 sfw_categories_inline_keyboard_builder = InlineKeyboardBuilder()
 for sfw_category in ImageCategories['sfw']:
     sfw_categories_inline_keyboard_builder.button(
         text=sfw_category,
-        callback_data=WaifuCategoryCallbackData(category=sfw_category)
-    )  # Потом поменяем колбэк
+        callback_data=WaifuCategoryCallbackData(category=sfw_category).pack()
+    )
 sfw_categories_inline_keyboard = sfw_categories_inline_keyboard_builder.adjust(4).as_markup()
+
+
+# Клавиатура для nsfw
+nsfw_categories_inline_keyboard_builder = InlineKeyboardBuilder()
+for nsfw_category in ImageCategories['nsfw']:
+    nsfw_categories_inline_keyboard_builder.button(
+        text=nsfw_category,
+        callback_data=WaifuCategoryCallbackData(category=nsfw_category).pack()
+    )
+nsfw_categories_inline_keyboard = nsfw_categories_inline_keyboard_builder.adjust(4).as_markup()
+
 
 waifu_type_inline_keyboard = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(
-        text='sfw',
-        callback_data=WaifuTypeCallbackData(type='sfw')
+        text='Обычные',
+        callback_data=WaifuTypeCallbackData(type='sfw').pack()
     ), InlineKeyboardButton(
-        text='nsfw',
-        callback_data=WaifuTypeCallbackData(type='nsfw')
+        text='18+',
+        callback_data=WaifuTypeCallbackData(type='nsfw').pack()
     )]
 ])
--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import config  # Config
-from handlers import common
+from handlers import common, waifu
 
 
 def register_all_routers(dp: Dispatcher):
     dp.include_router(common.common_router)
+    dp.include_router(waifu.waifu_router)
 
 
 async def main():
